2dd9df5fdebcf3d2ef0586c7f582b1cae9194ab3
Markus Mayer <widemeadows@gmail.com>
2024-05-23 19:16:16 +0200

    Add note about string generation and percent encoding

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@ use std::fmt::{Display, Formatter};
 use std::rc::Rc;
 use std::sync::Arc;
 
-use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
+use percent_encoding::{AsciiSet, CONTROLS, utf8_percent_encode};
 
 /// https://url.spec.whatwg.org/#fragment-percent-encode-set
 const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
@@ -250,7 +250,11 @@ impl Display for QueryString {
                     write!(f, "&")?;
                 }
 
+                // We convert the value to a String here. Ideally we'd print it directly (skipping
+                // the intermediate string generation) but due to the required percent encoding
+                // this isn't immediately possible.
                 let value = pair.value.render();
+
                 write!(
                     f,
                     "{key}={value}",
@@ -305,9 +309,9 @@ impl Value {
     }
 
     pub fn lazy_fn<F, T>(func: F) -> Self
-    where
-        F: Fn() -> T + 'static,
-        T: ToString + 'static,
+        where
+            F: Fn() -> T + 'static,
+            T: ToString + 'static,
     {
         Value {
             value: Box::new(move || func().to_string()),
@@ -320,8 +324,8 @@ impl Value {
 }
 
 impl<T> From<Rc<T>> for Value
-where
-    T: ToString + 'static,
+    where
+        T: ToString + 'static,
 {
     fn from(value: Rc<T>) -> Self {
         Value::lazy_rc(value)
@@ -329,8 +333,8 @@ where
 }
 
 impl<T> From<Arc<T>> for Value
-where
-    T: ToString + 'static,
+    where
+        T: ToString + 'static,
 {
     fn from(value: Arc<T>) -> Self {
         Value::lazy_arc(value)
@@ -338,9 +342,9 @@ where
 }
 
 impl<F, T> From<F> for Value
-where
-    F: Fn() -> T + 'static,
-    T: ToString + 'static,
+    where
+        F: Fn() -> T + 'static,
+        T: ToString + 'static,
 {
     fn from(value: F) -> Self {
         Value::lazy_fn(value)
